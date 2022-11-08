a58768bed485d2d8fd1278e04fab87f4dd2876ab
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-11-08 15:13:25 -0300

    infra(liviness): adding liviness probe

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ const express = require('express');
 
 const app = express();
 
+const startedAt = new Date().getTime();
+
 app.use(express.json());
 
 app.get('/', (_, res) =>
   res.send(`<h1>Running ${process.env.ENV_TO_TEST_CONFIG_FILES}</h1>`)
 );
-
+ 
 app.get('/secret', (_, res) => res.send(`<h1>${process.env.SECRET}</h1>`));
 
+app.get('/health', (_, res) => {
+  const now = new Date().getTime();
+
+  const timeAlive = (now - startedAt) / 1000;
+
+  if (timeAlive > 15) return res.sendStatus(200);
+  return res.sendStatus(500);
+});
 app.listen(80);
