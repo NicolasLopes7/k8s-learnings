a0cecc34fce46c0d0172dbccf0321d7a4dcdf93a
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-11-08 14:48:32 -0300

    infra(k8s): secret

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (_,res) => res.send('<h1>Running</h1>'))
+app.get('/', (_, res) =>
+  res.send(`<h1>Running ${process.env.ENV_TO_TEST_CONFIG_FILES}</h1>`)
+);
+
+app.get('/secret', (_, res) => res.send(`<h1>${process.env.SECRET}</h1>`));
 
 app.listen(80);
